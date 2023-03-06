74b0e1b719bfc86121c974ea4fd3bb1f2c0010e7
Yuhao Han <harryhan912@gmail.com>
2023-03-06 20:16:23 +0000

    dijkstra implementation

--- algo/astar/astar.cpp
+++ algo/astar/astar.cpp
@@ -2,6 +2,9 @@
 #include <map>
 #include <vector>
 
+#define WDT 7
+#define HET 6
+
 using namespace std;
 
 struct Node{
@@ -10,11 +13,19 @@ struct Node{
 	int val;
 	int num;
 	struct Node * from;
+	int fromx, fromy;
+	Node(){
+	}
+	Node(Node * a){
+		from = a;
+		val = 100000;
+	}
 	Node(Node * a, int xval, int yval, int valval){
 		x = xval;
 		y = yval;
 		val = valval;
-		from = a;
+		fromx = a->x;
+		fromy = a->y;
 		num = (a->num)+1;
 	}
 	Node(int xval, int yval, int valval){
@@ -33,9 +44,9 @@ struct Node{
 		this->y = a->y;
 		this->val = a->val;
 		this->from = a->from;
+		this->num = a->num;
 	}
 };
-
 
 bool sameCoor(Node * a, Node * b){
 	return 1 ? (a->x==b->x)&&(a->y==b->y) : 0;
@@ -55,61 +66,103 @@ void pushpq(vector<Node>& pq,Node * node){
 	return;
 }
 
-int main(){
-	int matrix [7][6] {0};
+void astar(Node * start, Node * end){
+	int matrix [WDT][HET] {0};
 	for (int i=0; i<7; ++i){
 		for (int j=0; j<6; ++j) matrix[i][j]=1;
 	}
+	matrix[5][1] = 10000;
+	matrix[1][1] = 10000;
+	matrix[4][2] = 10000;
+	matrix[6][2] = 10000;
+	matrix[0][3] = 10000;
+	matrix[4][3] = 10000;
+	matrix[1][4] = 10000;
+	matrix[4][5] = 10000;
+
 	Node null(-1,-1,-1);
-	Node start(&null, 0,0,0);
-	Node end(&null, 6,5,0);
-	Node cur(&null, 0,0,0);
-	cur.num=1;
-	vector<Node> pq;
-	vector<Node> buffer;
-	int counter = 0;
-	while(!sameCoor(&cur, &end)){
-		cout<<"cur: ";
-		cur.printNode();
-		buffer.push_back(Node(&cur, cur.x-1, cur.y,(6-cur.x)*(6-cur.x)+(5-cur.y)+(5-cur.y)+ cur.val+matrix[cur.x][cur.y]));
-		buffer.push_back(Node(&cur, cur.x+1, cur.y, (6-cur.x)*(6-cur.x)+(5-cur.y)+(5-cur.y)+cur.val+matrix[cur.x][cur.y]));
-		buffer.push_back(Node(&cur, cur.x, cur.y+1, (6-cur.x)*(6-cur.x)+(5-cur.y)+(5-cur.y)+cur.val+matrix[cur.x][cur.y]));
-		buffer.push_back(Node(&cur, cur.x, cur.y-1, (6-cur.x)*(6-cur.x)+(5-cur.y)+(5-cur.y)+cur.val+matrix[cur.x][cur.y]));
-		for(Node &i: buffer){
-			if (sameCoor(cur.from, &i)){
-				continue;
-			}
-			if(((i.x>=0)&&(i.y>=0)&&(i.x<=6)&&(i.y<=5))&&(!sameCoor(cur.from, &i)))
-				pushpq(pq, &i);
+	Node *nodeMatrix[WDT][HET];
+	
+	for(int i=0; i<WDT; i++){
+		for(int j=0; j<HET; j++){
+			nodeMatrix[i][j]=new Node(&null);
+			nodeMatrix[i][j]->x=i;
+			nodeMatrix[i][j]->y=j;
 		}
-		cur.copy(&pq[0]);
-		pq.erase(pq.begin());
-		// if ((cur.x+cur.y)>=11) break;
-		// if (counter>8) break;
-		// for (Node &i:pq) i.printNode();
-		// cout<<endl;
-		counter++;
-		buffer.clear();
 	}
-	// for (Node &i:pq) i.printNode();
-	Node try1(&null, 1,2,3);
-	Node try2(&null, 1,2,3);
-	pushpq(pq, &start);	
-	pushpq(pq, &end);	
-	pushpq(pq, &try1);	
-	pushpq(pq, &try2);	
-	Node a(&cur, cur.x-1, cur.y,(6-cur.x)*(6-cur.x)+(5-cur.y)+(5-cur.y)+ cur.val+matrix[cur.x][cur.y]);
-	// pq.push_back(Node(&cur, cur.x-1, cur.y,(6-cur.x)*(6-cur.x)+(5-cur.y)+(5-cur.y)+ cur.val+matrix[cur.x][cur.y]));
-	pq.push_back(a);
-	// cur.copy(&pq[0]);
-	pq.push_back(Node(&cur, cur.x-1, cur.y,(6-cur.x)*(6-cur.x)+(5-cur.y)+(5-cur.y)+ cur.val+matrix[cur.x][cur.y]));
+	// for(int i=0; i<WDT; i++){
+	// 	for(int j=0; j<HET; j++){
+	// 		cout<<i*HET+j<<": "; nodeMatrix[i][j]->printNode();
+	// 	}
+	// }
+	int curCo [2] = {start->x, start->y};
+	nodeMatrix[start->x][start->y]->val=0;
+	nodeMatrix[start->x][start->y]->num=0;
+	vector<Node> pq;
+	vector<int*> buffer;
+	int counter = 0;
+	
+	while((curCo[0]!=end->x)||(curCo[1]!=end->y)){
+		buffer.push_back(new int[2]{curCo[0]+1,curCo[1]});
+		buffer.push_back(new int[2]{curCo[0]-1,curCo[1]});
+		buffer.push_back(new int[2]{curCo[0],curCo[1]+1});
+		buffer.push_back(new int[2]{curCo[0],curCo[1]-1});
+		for (auto i:buffer){
+			cout<<"Current buffer: "<<"x: "<<*i<<"; y: "<<*(i+1)<<endl;
+		}
+		for (auto i:buffer){
+			if((i[0]>=0)&&(i[1]>=0)&&i[0]<WDT&&i[1]<HET
+				 &&((i[0]!=nodeMatrix[curCo[0]][curCo[1]]->fromx)
+				 ||(i[1]!=nodeMatrix[curCo[0]][curCo[1]]->fromy))
+				&&(nodeMatrix[curCo[0]][curCo[1]]->val+1<nodeMatrix[i[0]][i[1]]->val)
+			){
+				nodeMatrix[i[0]][i[1]]->val = matrix[i[0]][i[1]]+nodeMatrix[curCo[0]][curCo[1]]->val; //Insert heuristic here
+				nodeMatrix[i[0]][i[1]]->num = nodeMatrix[curCo[0]][curCo[1]]->num+1;
+				nodeMatrix[i[0]][i[1]]->fromx = curCo[0];
+				nodeMatrix[i[0]][i[1]]->fromy = curCo[1];
+				pushpq(pq, nodeMatrix[i[0]][i[1]]);
+				// pq.push_back(*nodeMatrix[i[0]][i[1]]);
+			}
+		}
+		for (auto i:buffer){
+			delete[] i;
+		}
+		buffer.clear();
+		for (auto i:pq){
+			i.printNode();
+		}
+		cout<<endl;
+		curCo[0] = pq[0].x;
+		curCo[1] = pq[0].y;
+		pq.erase(pq.begin());
+		cout<<"curCo: "<<curCo[0]<<", "<<curCo[1]<<endl;
+		cout<<"num: "<<nodeMatrix[curCo[0]][curCo[1]]->num<<endl;
+		// if(counter++>10) break;
+	}
+	for (int i=0; i<HET; i++){
+		for(int j=0; j<WDT; j++){
+			cout<<" "<<nodeMatrix[j][i]->fromx<<","<<nodeMatrix[j][i]->fromy<<" ";
+		}
+		cout<<endl;
+	}
+	
 
-	for (Node &i:pq) i.printNode();
+	int a = curCo[0];int b=curCo[1];
 
-	vector<int> vecint(2,3); 
-	// auto iter = vecint.begin()+1;
-	// iter = vecint.insert(iter, 20);
-	// // vecint.insert(iter, 30);
-	// for (int i: vecint) cout<<i<<" "<<endl;
+	while(a!=start->x||b!=start->y){
+		cout<<a<<", "<<b<<endl;
+		int ta = a;
+		int tb = b;
+		a = nodeMatrix[ta][tb]->fromx; b=nodeMatrix[ta][tb]->fromy;
+	}
+	cout<<start->x<<", "<<start->y<<endl;
+	return;
+}
+
+int main(){
+	Node start(6,1,0);
+	Node end(WDT-1, HET-1, 0);
+	astar(&start, &end);
+	
 	return 0;
 }
