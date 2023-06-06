253cf91905cf89a7a8696071ade10cb11fda9e07
harryhanYuhao <s2162783@ed.ac.uk>
2023-06-06 22:18:47 +0800

    haskell

--- miscellanea/haskell/quickstart/test.hs
+++ miscellanea/haskell/quickstart/test.hs
@@ -5,9 +5,23 @@ sayHello x = putStrLn("Hello, " ++ x ++ "!")
 
 triple x = x * 3
 
-y = 3 
+y = 2
 x = 4
 
 main :: IO ()
 main = 
-  do print((triple y))
+  do
+    let z = 1 + (23 `rem` 7)
+    -- show() function converts it to printalbe string`
+    -- '++' is string concatenation 
+    print("`rem` make function an infix: " ++ show(z))
+    let z = 1 + (rem 23 7)
+    print("No infix: " ++ show z)
+
+    print("x/y = " ++ show(x/y))
+    print("(/)x y = " ++ show((/) x y))
+
+    -- lambda 
+    -- lambda x.x 
+    let double = \x ->  x + 10 
+    print (identity(4))
